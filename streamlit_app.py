b3ebee1275ad3e54738736ca6fb4e492c9a7abb1
Vinaygit761 <kattamguri.v.goud@kipi.ai>
2024-10-15 23:17:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ if ingredients_list:
 for fruit_chosen in ingredients_list:
    ingredients_string += fruit_chosen + ''
  #st.write(ingredients_list)
-       my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+ my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
      st.write(my_insert_stmt)
      st.stop()
