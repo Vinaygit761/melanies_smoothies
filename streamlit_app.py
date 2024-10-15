bf0636264d4bca31d40af1bf5d0da0a0f4b344e5
Vinaygit761 <kattamguri.v.goud@kipi.ai>
2024-10-15 23:13:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,23 +21,17 @@ ingredients_list = st.multiselect(
 )
 if ingredients_list:
     ingredients_string = ''
-
-     for fruit_chosen in ingredients_list:
+         for fruit_chosen in ingredients_list:
          ingredients_string += fruit_chosen + ''
-
-     #st.write(ingredients_list)
-
-     my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
+ #st.write(ingredients_list)
+       my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
- 
      st.write(my_insert_stmt)
      st.stop()
     #st.write(my_insert_stmt)   
 time_to_insert = st.button("Submit Order")
-    
 if time_to_insert:
- session.sql(my_insert_stmt).collect()
-         
+ session.sql(my_insert_stmt).collect() 
 st.success('Your Smoothie is ordered!', icon="✅")
 
 
