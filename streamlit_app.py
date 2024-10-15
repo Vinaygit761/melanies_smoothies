c82046e4aa3e7700f3075f64a78691e3407d9c82
Vinaygit761 <kattamguri.v.goud@kipi.ai>
2024-10-15 23:19:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,9 +26,9 @@ for fruit_chosen in ingredients_list:
  #st.write(ingredients_list)
    my_insert_stmt = """ insert into smoothies.public.orders(ingredients)
             values ('""" + ingredients_string + """','"""+name_on_order +"""')"""
-     st.write(my_insert_stmt)
-     st.stop()
-    #st.write(my_insert_stmt)   
+  st.write(my_insert_stmt)
+  st.stop()
+ #st.write(my_insert_stmt)   
 time_to_insert = st.button("Submit Order")
 if time_to_insert:
  session.sql(my_insert_stmt).collect() 
